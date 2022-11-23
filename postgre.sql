c944674dca726db0e1d4f292d678e6115b2f7849
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2022-11-23 12:49:46 +0200

    Create postgre.sql

---
+++ postgre.sql
@@ -0,0 +1,18 @@
+# Step 1 – Installing the Components from the Ubuntu Repositories
+# sudo apt update
+# sudo apt install python3-pip python3-dev libpq-dev postgresql postgresql-contrib
+
+# Step 2 – Creating a Database and Database User
+# First, log into an interactive Postgres session by typing: sudo -u postgres psql
+
+CREATE DATABASE db;
+CREATE USER radwan WITH PASSWORD 'password';
+ALTER ROLE radwan SET client_encoding TO 'utf8';
+ALTER ROLE radwan SET default_transaction_isolation TO 'read committed';
+ALTER ROLE radwan SET timezone TO 'UTC';
+GRANT ALL PRIVILEGES ON DATABASE db TO radwan;
+\q
+
+# Now, install Django within a Virtual Environment using poetry and add psycopg2
+# Then, configure the Django Database Settings
+# Finally, migrate the Database and Test your Project
