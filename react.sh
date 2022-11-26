dec01b049f4629c808054d3a61dcef5af3af7245
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2022-11-26 17:50:07 +0200

    Update react.sh

--- react.sh
+++ react.sh
@@ -3,9 +3,14 @@
 # check version of [Node Version Manager](https://github.com/nvm-sh/nvm)
 nvm -v
 
+# examples:
+nvm use 18
+nvm use 18.12.1
+nvm install 18.12.1
+
 nvm install node
 
-node - v
+node -v
 
 # list available versions using ls-remote
 nvm ls-remote
@@ -21,6 +26,12 @@ npm -v
 # create a new react app called my-app
 npx create-react-app my-app
 
+# add typescript
+npx create-react-app my-app --template typescript
+
 cd my-app
 
 npm start
+
+# to add TypeScript to an existing Create React App project, first install it:
+npm install --save typescript @types/node @types/react @types/react-dom @types/jest
