255f34df7e5e9a04e7659330c848778af0b48077
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2022-12-22 17:20:52 +0200

    Update python.sh

--- python.sh
+++ python.sh
@@ -2,16 +2,16 @@
 
 # Installing Python 3.8.16 on WSL Ubuntu
 
-# First from PowerShell
+# First, from PowerShell
 wsl --set-version Ubuntu 1
  
-# Then
+# Then, from Ubuntu terminal
 sudo apt update 
 sudo apt install gcc make zlib1g-dev -y
 
 wget https://www.python.org/ftp/python/3.8.16/Pyhon-3.8.16.tgz
 tar -xzvf Python-3.8.16.tgz
 cd Python-3.8.16
-sudo ./configure 
+./configure --enable-optimizations
 sudo make 
 sudo make altinstall
