59ff888165e90c05387b83e09cf00959c51dbf1d
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2022-12-24 13:51:51 +0200

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -39,3 +39,4 @@ sudo systemctl status docker
 # Error: Got permission denied while trying to connect to the Docker daemon socket at unix:///var/run/docker.sock: 
 # Post "http://%2Fvar%2Frun%2Fdocker.sock/v1.24/auth": dial unix /var/run/docker.sock: connect: permission denied
 # Fix: https://www.digitalocean.com/community/questions/how-to-fix-docker-got-permission-denied-while-trying-to-connect-to-the-docker-daemon-socket
+# Fix: https://linuxhandbook.com/docker-permission-denied/
