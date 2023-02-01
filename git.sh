391f0468957ac463416330f45260258b5b33bc57
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2023-02-01 14:19:49 +0400

    Update git.sh

--- git.sh
+++ git.sh
@@ -16,11 +16,14 @@ git fetch
 
 git branch
 
-# add 
+# add
 git branch last-update
 
 # switch
 git checkout last-update
+
+# set the local branch to track the remote branch
+git branch --set-upstream-to=origin/last-update last-update
 
 # Undoing last commit (not pushed) to return to the staging area
 git reset --soft HEAD~
